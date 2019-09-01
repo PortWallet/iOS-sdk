90ee5029b0af9058e39c243417dc876cb83bcebb
Zahedul Alam <z.alam015@gmail.com>
2019-09-01 12:47:12 +0600

    create iOS project

---
+++ example/SDKExample/SDKExample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SDKExample
+//
+//  Created by Zahedul Alam on 1/9/19.
+//  Copyright © 2019 Zahedul Alam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
